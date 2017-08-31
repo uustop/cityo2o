b5bfa5bdb14a0eb21b5a3a2085f13995b274955d
uustop <yth3412@163.com>
2017-08-31 14:10:07 +0800

    add swiper

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # cityo2o
---同城逛React Native App
---react-native init cityo2o --version 0.44.3
---yarn add react-navigation
-##android配置echo 'export PATH=$PATH:~/androidsdk/android-sdk-macosx/platform-tools/' >> ~/.bash_profile
-##source ~/.bash_profile
-##adb reverse tcp:8081 tcp:8081
-#添加资源图片
+###同城逛 React Native App
+
+react-native init cityo2o --version 0.44.3
+
+yarn add react-navigation
+
+####android配置echo 'export PATH=$PATH:~/androidsdk/android-sdk-macosx/platform-tools/' >> ~/.bash_profile
+
+####source ~/.bash_profile
+
+####adb reverse tcp:8081 tcp:8081
+
+####添加资源图片
+
+####swiper 仿京东
--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,32 +1,80 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class Homepage extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to homepage!
-        </Text>
-      </View>
-    );
-  }
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    Dimensions,
+} from 'react-native'
+import Swiper from 'react-native-swiper'
+const { width } = Dimensions.get('window')
+const styles = {
+    wrapper: {
+    },
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'transparent'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    image: {
+        width,
+        flex: 1
+    },
+    paginationStyle: {
+        position: 'absolute',
+        bottom: 10,
+        right: 10
+    },
+    paginationText: {
+        color: 'white',
+        fontSize: 20
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+export default class Homepage extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            imgList: [
+                'https://img1.360buyimg.com/da/jfs/t8377/156/202307/110069/1e8d4ee4/599e8949Na8c4b4ca.jpg',
+                'https://m.360buyimg.com/mobilecms/s720x322_jfs/t8422/318/375531685/83272/ff93867f/59a6ec9aN11a4988e.jpg!q70.jpg',
+                'https://m.360buyimg.com/mobilecms/s720x322_jfs/t7801/235/1918577840/66825/a8ce655a/59a3bad2Nab4833e2.jpg!q70.jpg',
+                'https://m.360buyimg.com/mobilecms/s720x322_jfs/t9424/48/50001665/86067/98db6c33/599f9b5fNc846be47.jpg!q70.jpg'
+            ],
+            adList:[
+                'https://m.360buyimg.com/mobilecms/jfs/t9154/69/384413561/61373/8889e7f6/59a6dab1Nccab65c8.jpg!q70.jpg'
+            ]
+        }
+    }
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <View style={{height:168}}>
+                    <Swiper>
+                        <View style={styles.slide}>
+                            <Image style={styles.image} source={{url:this.state.imgList[0]}} />
+                        </View>
+                        <View style={styles.slide}>
+                            <Image style={styles.image} source={{url:this.state.imgList[1]}} />
+                        </View>
+                        <View style={styles.slide}>
+                            <Image style={styles.image} source={{url:this.state.imgList[2]}} />
+                        </View>
+                        <View style={styles.slide}>
+                            <Image style={styles.image} source={{url:this.state.imgList[3]}} />
+                        </View>
+                    </Swiper>
+                </View >
+                <View style={{height:96}}>
+                    <Image style={styles.image} source={{url:this.state.adList[0]}} />
+                </View>
+            </View>
+        );
+    }
+}
+
